94a61e882612844f1402e042e772dbdd9572a55f
Rashid Lasker <rashidlasker@gmail.com>
2017-04-12 16:03:15 -0400

    started the site

---
+++ main.js
@@ -0,0 +1,12 @@
+ function mouseoverSize(){
+    $( "#effect" ).animate({
+       backgroundColor: "#fff",
+       color: "#000",
+       width: 240
+    }, 1000 );
+}
+
+function fadeIn(){
+	$('#ye').hide();
+	$('#ye').fadeIn(1000);
+}
